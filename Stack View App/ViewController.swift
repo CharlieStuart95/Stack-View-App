9b352e55ac081ab049859110ba9db36b2e170b4f
Charlie Stuart <charliestuart95@gmail.com>
2017-02-12 16:26:28 +0000

    Initial Commit

---
+++ Stack View App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stack View App
+//
+//  Created by Charlie Stuart on 12/02/2017.
+//  Copyright © 2017 Charlie Stuart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
